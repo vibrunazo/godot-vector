8322b7ccae1946cb365b68c003c7778fae3d315c
Vandré Brunazo <vibrunazo@gmail.com>
2023-08-04 18:02:03 -0300

    grid colors and width

--- Grid.gd
+++ Grid.gd
@@ -2,6 +2,10 @@ extends Control
 
 var cell_size := 16
 var gcolor := Color(0.25, 0.2, 0.3)
+var pcolor := Color(0.1, 0.1, 0.35)
+var dcolor := Color(0.05, 0.05, 0.2)
+var primary_every := 5
+var double_every := 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,16 +21,36 @@ func _draw():
 	draw_grid()
 
 func draw_grid():
-	for n in range(1, 100):
-		draw_row(n)
-		draw_col(n)
+	for n in range(1, 160):
+		draw_row(n, 1)
+		draw_col(n, 1)
+	for n in range(1, 160/primary_every):
+		draw_row(n, primary_every)
+		draw_col(n, primary_every)
+	for n in range(0, 160/double_every):
+		draw_row(n, double_every)
+		draw_col(n, double_every)
 
-func draw_row(n: int):
-	var from := Vector2(0, n * cell_size)
-	var to:= Vector2(1600, n * cell_size)
-	draw_line(from, to, gcolor, 1)
+func draw_row(n: int, e: int):
+	var from := Vector2(0, n * cell_size * e)
+	var to:= Vector2(2000, n * cell_size * e)
+	draw_segment(from, to, e, n)
 
-func draw_col(n: int):
-	var from := Vector2(n * cell_size, 0)
-	var to:= Vector2(n * cell_size, 1600)
-	draw_line(from, to, gcolor, 1)
+func draw_col(n: int, e: int):
+	var from := Vector2(n * cell_size * e, 0)
+	var to:= Vector2(n * cell_size * e, 2000)
+	draw_segment(from, to, e, n)
+
+func draw_segment(from, to, e, n):
+	if e == 1 and not (n % primary_every == 0):
+		draw_line(from, to, gcolor, 1)
+	elif e == primary_every and not (n % (double_every / primary_every) == 0):
+		draw_line(from, to, pcolor, 1)
+	elif e == double_every:
+		draw_line(from, to, dcolor, 2)
+#	if n % double_every == 0:
+#		draw_line(from, to, dcolor, 2)
+#	elif n % primary_every == 0:
+#		draw_line(from, to, pcolor, 1)
+#	else:
+#		draw_line(from, to, gcolor, 1)
