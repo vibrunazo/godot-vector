5ee817635ba739770299f6b897ec631fe34383e7
Vandré Brunazo <vibrunazo@gmail.com>
2023-08-24 15:03:13 -0300

    show fps in debug hud

---
+++ scenes/debug_ui.gd
@@ -0,0 +1,21 @@
+extends Control
+
+@onready var label: RichTextLabel = %Label
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if visible:
+		update_text()
+
+
+func _unhandled_key_input(event):
+	if event.is_action_pressed("debug"):
+		visible = !visible
+		
+func update_text():
+	label.text = 'FPS: %s' % Engine.get_frames_per_second()
